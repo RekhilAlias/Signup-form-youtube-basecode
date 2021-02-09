44c1707b9fb31973578e59fd3222bc30b3638bf0
Rekhil elias paulose <rekhilelias2014@gmail.com>
2021-02-09 17:04:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyAkh5_zm7jS99U5t9DxMh8Rdx1JuCo0g_0",
-    authDomain: "dataform-19543.firebaseapp.com",
-    projectId: "dataform-19543",
-    storageBucket: "dataform-19543.appspot.com",
-    messagingSenderId: "973463754545",
-    appId: "1:973463754545:web:40ad9830ca2e0bf5d4c210"
+    apiKey: "add",
+    authDomain: "add",
+    projectId: "add",
+    storageBucket: "add",
+    messagingSenderId: "add",
+    appId: "add"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
